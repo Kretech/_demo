6e00c1d5c3f3560fb43749d90a7a3a1d6bb79688
huiren <zhrlnt@gmail.com>
2018-10-30 22:15:04 +0800

    service register

--- cli_say_hi/main.go
+++ cli_say_hi/main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"fmt"
-
 	"bytes"
+	"fmt"
+	"os"
 
+	"github.com/Kretech/framework/config"
 	. "github.com/Kretech/framework/foundation"
 	"github.com/Kretech/framework/foundation/console"
 )
 
 func init() {
 	app := App()
+
+	app.RegisterService(config.NewTomlFileServiceRegister("conf.toml"))
+
 	app.Singleton(`console`, console.NewKernel())
 }
 
 func main() {
+
 	console := App().Make(`console`).(*console.Kernel)
+
 	console.RegisterCommand(&SayHiCmd{})
+	console.RegisterCommand(&HiWebCmd{})
 
 	out := bytes.NewBuffer([]byte{})
-	console.Handle(out)
+	console.Handle(os.Args, out)
 
 	fmt.Print(out)
 }
